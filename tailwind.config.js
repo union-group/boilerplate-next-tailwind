a1d503b5b0bcc68bc6904e19003dac4a043fcaa2
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-07-23 00:23:08 -0300

    feat: add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   mode: 'jit',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#851d86',
+        secondary: '#0048fe',
+      },
+      borderRadius: {
+        10: '10px',
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
